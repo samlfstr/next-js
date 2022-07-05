a125ddce3c035c589e8a21e1b58d8bdb23ecbab2
Aaron <samlfstr@gmail.com>
2022-07-05 14:55:15 +0300

    Dyn route

--- pages/text.js
+++ pages/text.js
@@ -1,9 +1,12 @@
 import styles from '../styles/Text.module.css';
+import { useRouter } from 'next/router'
 
 export default function Text(){
+    const router = useRouter()
+    const { name } = router.query
     return(
         <div className="grid place-items-center h-screen">
-            <p> My name is Cengiz Han</p>
+            <p> My name is {name}</p>
         </div>
     )
 }
