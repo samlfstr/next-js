6e6c47feee0c32b0b299ecbe20eb1f8d6a810cd8
Aaron <samlfstr@gmail.com>
2022-07-05 14:38:44 +0300

    First Deploy

---
+++ pages/text.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Text.module.css';
+
+export default function Text(){
+    return(
+        <div className="grid place-items-center h-screen">
+            <p> My name is Cengiz Han</p>
+        </div>
+    )
+}
