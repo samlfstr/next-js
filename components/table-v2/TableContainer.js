61a9e3463455774f59b5b3cd77feb66284535df8
Aaron <samlfstr@gmail.com>
2022-07-10 22:21:01 +0300

    Added sorting to the table

--- components/table-v2/TableContainer.js
+++ components/table-v2/TableContainer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {useTable} from "react-table";
-import TableData from "./TableData";
+import { useSortBy } from "react-table";
 
 export default function TableContainer({columns, data}) {
 
@@ -15,10 +15,16 @@ export default function TableContainer({columns, data}) {
     {
       columns,
       data,
-    }
+    },
+    useSortBy
   );
   console.log(getTableProps())
   console.log(getTableBodyProps())
+
+  function generateSortingIndicator(column) {
+    return column.isSorted ? (column.isSortedDesc ? " ↑ " : " ↓ ") : "";
+  }
+
   return (
     <table className="uppercase" {...getTableProps()}>
       <thead>
@@ -28,8 +34,13 @@ export default function TableContainer({columns, data}) {
             <tr className="bg-gray-400" {...headerGroup.getHeaderGroupProps()} >
               {
                 headerGroup.headers.map((column) => (
+
+                    // <th className="border-2 border-black p-2" {...column.getHeaderProps()}>{column.render("Header")}</th>
                     // eslint-disable-next-line react/jsx-key
-                    <th className="border-2 border-black p-2" {...column.getHeaderProps()}>{column.render("Header")}</th>
+                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                      {column.render("Header")}
+                      {generateSortingIndicator(column)}
+                    </th>
                   )
                 )
               }
@@ -40,18 +51,20 @@ export default function TableContainer({columns, data}) {
       </thead>
       <tbody {...getTableBodyProps()}>
       {
-        rows.map(row => { prepareRow(row)
-        return (
-          // eslint-disable-next-line react/jsx-key
-          <tr className="bg-gray-200" {...row.getRowProps()}>
-            {
-              row.cells.map(cell => {
-                // eslint-disable-next-line react/jsx-key
-                return <td className="border-2 border-black p-2" {...cell.getCellProps()}> {cell.render("Cell")} </td>})
-            }
-          </tr>
-        )
-      })}
+        rows.map(row => {
+          prepareRow(row)
+          return (
+            // eslint-disable-next-line react/jsx-key
+            <tr className="bg-gray-200" {...row.getRowProps()}>
+              {
+                row.cells.map(cell => {
+                  // eslint-disable-next-line react/jsx-key
+                  return <td className="border-2 border-black p-2" {...cell.getCellProps()}> {cell.render("Cell")} </td>
+                })
+              }
+            </tr>
+          )
+        })}
       </tbody>
     </table>
   )
